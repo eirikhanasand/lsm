1d8984b5207f59ef18d85bde59df744919fce06b
eirikhanasand <eirik.hanasand@gmail.com>
2025-01-15 18:26:37 +0100

    Began work on terraform

---
+++ tests/terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "4.3.0"
+        }
+    }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.5.0"
+
+  name = "test-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "s3_bucket" {
+  source  = "github.com/terraform-aws-modules/terraform-aws-s3-bucket"
+  
+  bucket = "example-artifactory-test-bucket"
+  acl    = "private"
+}
