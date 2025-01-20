20f652a2a872274b1e4029cd684cdffaac072f35
sindre <sindrhst@stud.ntnu.no>
2025-01-20 18:58:09 +0100

    Fixed gradle testing
    Gradle testing now uses ENV variables.

---
+++ tests/gradle/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.jfrog.artifactory") version "5.+"
+    kotlin("jvm") version "1.9.22"
+}
+
+buildscript {
+    repositories {
+        maven {
+            url = uri("https://" + System.getenv("JFROG_TRIAL_ID") + "/java")
+            credentials {
+                username = System.getenv("JFROG_USERNAME")
+                password = System.getenv("JFROG_ACCESS_TOKEN")
+            }
+        }
+    }
+    dependencies {
+        classpath("org.jfrog.buildinfo:build-info-extractor-gradle:4+")
+    }
+}
+
+allprojects {
+    repositories {
+        maven {
+            url = uri("https://" + System.getenv("JFROG_TRIAL_ID") + "/java")
+            credentials {
+                username = System.getenv("JFROG_USERNAME")
+                password = System.getenv("JFROG_ACCESS_TOKEN")
+            }
+        }
+    }
+
+    apply(plugin = "com.jfrog.artifactory")
+}
+
+artifactory {
+    setContextUrl("https://" + System.getenv("JFROG_TRIAL_ID"))
+
+    publish {
+        repository {
+            repoKey = "java"
+            username = System.getenv("JFROG_USERNAME")
+            password = System.getenv("JFROG_ACCESS_TOKEN")
+        }
+    }
+}
+
+val dependencyFromArgs: String? = project.findProperty("dependency") as String?
+
+// Dynamically add dependency during configuration phase
+tasks.register("addDependencyAndBuild") {
+    doFirst {
+        if (dependencyFromArgs != null) {
+            project.dependencies.add("implementation", dependencyFromArgs)
+
+            configurations.compileClasspath.resolvedConfiguration.firstLevelModuleDependencies.forEach {
+                println("Resolved dependency: ${it.moduleGroup}:${it.moduleName}:${it.moduleVersion}")
+            }
+
+            println("Dependency added: $dependencyFromArgs")
+        }
+    }
+
+    doLast {
+        tasks.getByName("build").dependsOn(this)
+    }
+}
+
