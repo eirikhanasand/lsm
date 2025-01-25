c6606b36d8b68e6ba4a6058a2bab5aca97709bdf
eirikhanasand <eirik.hanasand@gmail.com>
2025-01-25 03:22:41 +0100

    Initial POC. Serving entire OSV db in 2-10ms

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# THIS IS THE API ENTRYPOINT. SEE /ui/entrypoint.sh FOR THE FRONTEND ENTRYPOINT.
+
 # Starts varnish
 varnishd -a :8080 -f /etc/varnish/default.vcl -s malloc,512m &
 
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,17 +1,19 @@
 import { FastifyInstance, FastifyPluginOptions } from "fastify"
-import putSample from "./put/sample.js"
 import indexHandler from "./get/index.js"
 import whitelistHandler from "./get/index.js"
-import osvHandler from "./post/osv.js"
+import osvHandler from "./get/osv.js"
+// import osvHandler from "./post/osv.js"
+// import putSample from "./put/sample.js"
 
 export default async function apiRoutes(fastify: FastifyInstance, options: FastifyPluginOptions) {
     // get handlers
     fastify.get("/", indexHandler)
     fastify.get("/whitelist", whitelistHandler)
+    fastify.get("/osv/:ecosystem/:name/:version", osvHandler)
     
     // post handlers
-    fastify.post("/osv", osvHandler)
+    // fastify.post("/osv", osvHandler)
 
     // put handlers
-    fastify.put("/", putSample)
+    // fastify.put("/", putSample)
 }
