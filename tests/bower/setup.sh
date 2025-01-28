b13dde3f4642b7688a17bdd1e18a53236a6603bd
eirikhanasand <eirik.hanasand@gmail.com>
2025-01-28 16:50:08 +0100

    bower

--- tests/bower/setup.sh
+++ tests/bower/setup.sh
@@ -9,4 +9,31 @@ if [[ -z "$JFROG_EMAIL" || -z "$JFROG_TOKEN" || -z "$JFROG_ID" ]]; then
   exit 1
 fi
 
-echo "Bower safe todo"
+# Installs Bower
+if ! command -v bower &> /dev/null; then
+  echo "Installing Bower..."
+  npm install bower
+fi
+
+
+
+
+# THIS SECTION CAN LIKELY BE REMOVED WHEN THE CONFIGURATION IS WORKING
+# Creates a Bower configuration file
+cat > .bowerrc <<EOL
+{
+  "directory": "bower_components",
+  "proxy": "https://$TRIAL_ID.jfrog.io/artifactory/github/",
+  "https-proxy": "https://$TRIAL_ID.jfrog.io/artifactory/github/"
+}
+EOL
+
+# Initializes the Bower project
+npx bower init --yes
+# THIS SECTION CAN LIKELY BE REMOVED WHEN THE CONFIGURATION IS WORKING
+
+
+
+
+# Installs jquery to test the configuration
+npx bower install jquery
