5634c662d20793e8d62042ab2fd9c1b4bd8c9fdc
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-03 02:09:33 +0100

    Continued swift testing

--- tests/swift/setup.sh
+++ tests/swift/setup.sh
@@ -46,8 +46,8 @@ semaphore.wait()
 EOL
 
 # Sets proxy environment variables
-export http_proxy="https://$JFROG_ID.jfrog.io/artifactory/github/"
-export https_proxy="https://$JFROG_ID.jfrog.io/artifactory/github/"
+export http_proxy="https://$JFROG_ID.jfrog.io/artifactory/api/github/github/"
+export https_proxy="https://$JFROG_ID.jfrog.io/artifactory/api/github/github/"
 
 # Build and run the Swift project
 swift build
