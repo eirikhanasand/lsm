30267082e23591357c964cb7cf15fb342c200d0e
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-03 02:39:08 +0100

    Completed ansible

--- tests/ansible/setup.sh
+++ tests/ansible/setup.sh
@@ -9,16 +9,5 @@ if [[ -z "$JFROG_EMAIL" || -z "$JFROG_TOKEN" || -z "$JFROG_ID" ]]; then
   exit 1
 fi
 
-# Exports proxy environment variables
-export http_proxy="https://$JFROG_ID.jfrog.io/artifactory/github/"
-export https_proxy="https://$JFROG_ID.jfrog.io/artifactory/github/"
-
-# Runs the playbook
-ansible-playbook -i inventory.ini test_proxy.yml
-
-# Verifies if the file was downloaded
-if [[ -f "/tmp/test_file.png" ]]; then
-  echo "File downloaded successfully via proxy."
-else
-  echo "Failed to download the file."
-fi
+# Runs the playbook with proxy settings
+ansible-playbook -i inventory.ini test_proxy.yml -e "JFROG_ID=$JFROG_ID" -e "JFROG_EMAIL=$JFROG_EMAIL" -e "JFROG_TOKEN=$JFROG_TOKEN"
