4ef44d80f53c50a9177b3c9f3420cec1fa603279
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-04 23:32:47 +0100

    Fixed inconsistencies

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,6 +1,19 @@
 import { FastifyInstance, FastifyPluginOptions } from "fastify"
+import indexHandler from "./get/index.js"
+import whitelistHandler from "./get/index.js"
 import osvHandler from "./get/osv.js"
+// import osvHandler from "./post/osv.js"
+// import putSample from "./put/sample.js"
 
 export default async function apiRoutes(fastify: FastifyInstance, options: FastifyPluginOptions) {
+    // get handlers
+    fastify.get("/", indexHandler)
+    fastify.get("/whitelist", whitelistHandler)
     fastify.get("/osv/:ecosystem/:name/:version", osvHandler)
+    
+    // post handlers
+    // fastify.post("/osv", osvHandler)
+
+    // put handlers
+    // fastify.put("/", putSample)
 }
