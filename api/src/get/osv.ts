47e188cabab96ad1a6ac293983a2e2b87fd1bc5a
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-07 19:37:24 +0100

    Working to resolve hidden bug

--- api/src/get/osv.ts
+++ api/src/get/osv.ts
@@ -1,45 +1,36 @@
 import { FastifyReply, FastifyRequest } from "fastify"
-import { Client } from "pg"
 import versionAffected from "../../utils/version.js"
+// import { get } from "../db.js"
 
-const dbConfig = {
-    user: "osvuser",
-    host: "postgres", 
-    database: "osvdb",
-    password: "osvpassword",
-    port: 5432
+type OSVHandlerParams = {
+    name: string
+    version: string
+    ecosystem: string
 }
 
-
 export default async function osvHandler(req: FastifyRequest, res: FastifyReply) {
-    const { name, version, ecosystem } = req.params as { name: string, version: string, ecosystem: string }
+    const { name, version, ecosystem } = req.params as OSVHandlerParams
 
     if (!name || !version || !ecosystem) {
         return res.status(400).send({ error: "Missing name, version, or ecosystem." })
     }
 
-    const client = new Client(dbConfig)
-    await client.connect()
-
     try {
-        const query = `
-            SELECT data FROM vulnerabilities 
-            WHERE ecosystem = $1 AND name = $2
-        `
-        const result = await client.query(query, [ecosystem, name])
+        // const result = await get(`
+        //     SELECT data FROM vulnerabilities 
+        //     WHERE name = $1 AND version = $2 AND ecosystem = $3
+        // `, [name, ecosystem, version])
 
-        if (result.rows.length === 0) {
-            return res.status(404).send({})
-        }
+        // if (result.rows.length === 0) {
+        //     return res.status(404).send({})
+        // }
 
-        const vulnerabilities = result.rows.map(row => row.data)
-        const filteredVulnerabilities = vulnerabilities.filter(vuln => versionAffected(version, ecosystem, vuln))
+        // const vulnerabilities = result.rows.map(row => row.data)
+        // const filteredVulnerabilities = vulnerabilities.filter(vuln => versionAffected(version, ecosystem, vuln))
 
-        return res.send(filteredVulnerabilities.length ? filteredVulnerabilities : {})
+        // return res.send(filteredVulnerabilities.length ? filteredVulnerabilities : {})
     } catch (error) {
         console.error("Database error:", error)
         return res.status(500).send({ error: "Internal Server Error" })
-    } finally {
-        await client.end()
     }
 }
