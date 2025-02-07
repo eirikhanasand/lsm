a341ae52ec3ce650dc20e7c7aad2a115d074b77a
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-07 19:39:48 +0100

    Working to resolve hidden bug

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,33 +1,33 @@
-// import dotenv from 'dotenv'
-// import { Pool } from "pg"
+import dotenv from 'dotenv'
+import { Pool } from "pg"
 
-// dotenv.config({ path: '../.env' })
+dotenv.config({ path: '../.env' })
 
-// const { DB_PASSWORD } = process.env
-// if (!DB_PASSWORD) {
-//     throw new Error("Missing DB_PASSWORD env variable.")    
-// }
+const { DB_PASSWORD } = process.env
+if (!DB_PASSWORD) {
+    throw new Error("Missing DB_PASSWORD env variable.")    
+}
 
-// const pool = new Pool({
-//     user: "osvuser",
-//     host: "pgbouncer",
-//     database: "osvdb",
-//     password: 'osvpassword',
-//     // password: DB_PASSWORD,
-//     port: 5432,
-//     max: 20,
-//     idleTimeoutMillis: 5000,
-//     connectionTimeoutMillis: 3000
-// })
+const pool = new Pool({
+    user: "osvuser",
+    host: "pgbouncer",
+    database: "osvdb",
+    password: 'osvpassword',
+    // password: DB_PASSWORD,
+    port: 5432,
+    max: 20,
+    idleTimeoutMillis: 5000,
+    connectionTimeoutMillis: 3000
+})
 
-// export async function get(query: string, params: string[]) {
-//     const client = await pool.connect()
-//     try {
-//         return await client.query(query, params)
-//     } catch (error) {
-//         client.release()
-//         throw error
-//     } finally {
-//         client.release()
-//     }
-// }
+export async function get(query: string, params: string[]) {
+    const client = await pool.connect()
+    try {
+        return await client.query(query, params)
+    } catch (error) {
+        client.release()
+        throw error
+    } finally {
+        client.release()
+    }
+}
