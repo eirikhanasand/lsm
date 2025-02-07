9467586b753b3718e9df27d05a11276bb14025a3
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-07 19:29:02 +0100

    Added tables for whitelisting and blacklisting

--- api/db/init.sql
+++ api/db/init.sql
@@ -15,7 +15,59 @@ BEGIN
     END IF;
 END $$;
 
+-- Whitelisted dependencies
+CREATE TABLE IF NOT EXISTS whitelist (
+    name TEXT PRIMARY KEY
+);
 
+-- Blacklisted dependencies
+CREATE TABLE IF NOT EXISTS blacklist (
+    name TEXT PRIMARY KEY
+);
+
+-- Table for version specific whitelisted dependencies
+CREATE TABLE IF NOT EXISTS whitelist_versions (
+    name TEXT NOT NULL,
+    version TEXT,
+    FOREIGN KEY (name) REFERENCES whitelist(name)
+);
+
+-- Table for ecosystem specific whitelisted dependencies
+CREATE TABLE IF NOT EXISTS whitelist_ecosystems (
+    name TEXT NOT NULL,
+    ecosystem TEXT,
+    FOREIGN KEY (name) REFERENCES whitelist(name)
+);
+
+-- Table for repository specific whitelisted dependencies
+CREATE TABLE IF NOT EXISTS whitelist_repositories (
+    name TEXT NOT NULL,
+    repository TEXT,
+    FOREIGN KEY (name) REFERENCES whitelist(name)
+);
+
+-- Table for version specific blacklisted dependencies
+CREATE TABLE IF NOT EXISTS blacklist_versions (
+    name TEXT NOT NULL,
+    version TEXT,
+    FOREIGN KEY (name) REFERENCES blacklist(name)
+);
+
+-- Table for ecosystem specific blacklisted dependencies
+CREATE TABLE IF NOT EXISTS blacklist_ecosystems (
+    name TEXT NOT NULL,
+    ecosystem TEXT,
+    FOREIGN KEY (name) REFERENCES blacklist(name)
+);
+
+-- Table for repository specific blacklisted dependencies
+CREATE TABLE IF NOT EXISTS blacklist_repositories (
+    name TEXT NOT NULL,
+    repository TEXT,
+    FOREIGN KEY (name) REFERENCES blacklist(name)
+);
+
+-- Creates vulnerability table
 CREATE TABLE IF NOT EXISTS vulnerabilities (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
@@ -24,5 +76,16 @@ CREATE TABLE IF NOT EXISTS vulnerabilities (
     data JSONB NOT NULL
 );
 
+-- Indexes for Vulnerability
 CREATE INDEX IF NOT EXISTS idx_ecosystem_name_version ON vulnerabilities (ecosystem, name, version);
 CREATE INDEX IF NOT EXISTS idx_vulnerability_name ON vulnerabilities (name);
+
+-- Indexes for Whitelist
+CREATE INDEX IF NOT EXISTS idx_whitelist_versions_name ON whitelist_versions (name);
+CREATE INDEX IF NOT EXISTS idx_whitelist_ecosystems_name ON whitelist_ecosystems (name);
+CREATE INDEX IF NOT EXISTS idx_whitelist_repositories_name ON whitelist_repositories (name);
+
+-- Indexes for Blacklist
+CREATE INDEX IF NOT EXISTS idx_blacklist_versions_name ON blacklist_versions (name);
+CREATE INDEX IF NOT EXISTS idx_blacklist_ecosystems_name ON blacklist_ecosystems (name);
+CREATE INDEX IF NOT EXISTS idx_blacklist_repositories_name ON blacklist_repositories (name);
