11d9e955e16c09e5e265fa3fcc9071ae631d77af
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-07 19:41:20 +0100

    Hidden bug resolved, unknown fix or problem.

--- api/src/get/whitelist.ts
+++ api/src/get/whitelist.ts
@@ -1,6 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify"
-// import versionAffected from "../../utils/version.js"
-// import { get } from "../db.js"
+import versionAffected from "../../utils/version.js"
+import { get } from "../db.js"
 
 type OSVHandlerParams = {
     name: string
@@ -16,20 +16,20 @@ export default async function whitelistHandler(req: FastifyRequest, res: Fastify
     }
 
     try {
-        // const result = await get(`
-        //     SELECT * FROM whitelist 
-        //     WHERE name = $1
-        //     AND version = $2 AND ecosystem = $3
-        // `, [name, ecosystem, version])
+        const result = await get(`
+            SELECT * FROM whitelist 
+            WHERE name = $1
+            AND version = $2 AND ecosystem = $3
+        `, [name, ecosystem, version])
 
-        // if (result.rows.length === 0) {
-        //     return res.status(404).send({})
-        // }
+        if (result.rows.length === 0) {
+            return res.status(404).send({})
+        }
 
-        // const vulnerabilities = result.rows.map(row => row.data)
-        // const filteredVulnerabilities = vulnerabilities.filter(vuln => versionAffected(version, ecosystem, vuln))
+        const vulnerabilities = result.rows.map(row => row.data)
+        const filteredVulnerabilities = vulnerabilities.filter(vuln => versionAffected(version, ecosystem, vuln))
 
-        // return res.send(filteredVulnerabilities.length ? filteredVulnerabilities : {})
+        return res.send(filteredVulnerabilities.length ? filteredVulnerabilities : {})
     } catch (error) {
         console.error("Database error:", error)
         return res.status(500).send({ error: "Internal Server Error" })
