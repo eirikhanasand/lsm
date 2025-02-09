d000fe038a094639f530a2cc9690161136daefbd
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-09 05:19:42 +0100

    Fixed bug 38 min -> 4.5 min

--- api/update_osv.sh
+++ api/update_osv.sh
@@ -28,28 +28,21 @@ find osv -name '*.json' -print0 | xargs -P 32 -0 -I {} sh -c '
 ' _ "$temp_file" "$temp_names"
 
 echo "Populating vulnerabilities..."
+$PSQL "CREATE TABLE vulnerabilities_new (name TEXT PRIMARY KEY, ecosystem TEXT, version TEXT, data JSONB);"
+$PSQL "\COPY vulnerabilities_new (name, ecosystem, version, data) FROM '$temp_file' WITH (FORMAT csv, DELIMITER ',', QUOTE '\"', ESCAPE '\"');"
+
 $PSQL_MULTILINE <<EOF
 BEGIN;
-CREATE TABLE IF NOT EXISTS vulnerabilities_new (
-    name TEXT PRIMARY KEY,
-    ecosystem TEXT,
-    version TEXT,
-    data JSONB
-);
-TRUNCATE vulnerabilities_new;
-\COPY vulnerabilities_new (name, ecosystem, version, data) FROM '$temp_file' WITH (FORMAT csv, DELIMITER ',', QUOTE '"', ESCAPE '"');
 ALTER TABLE vulnerabilities RENAME TO vulnerabilities_old;
 ALTER TABLE vulnerabilities_new RENAME TO vulnerabilities;
 DROP TABLE vulnerabilities_old;
 COMMIT;
 EOF
 
-
 echo "Populating vulnerability names..."
 $PSQL_MULTILINE <<EOF
 BEGIN;
-CREATE TABLE IF NOT EXISTS vulnerability_names_new (name TEXT UNIQUE);
-TRUNCATE vulnerability_names_new;
+CREATE TABLE vulnerability_names_new (name TEXT UNIQUE);
 \COPY vulnerability_names_new (name) FROM '$temp_names' WITH (FORMAT csv);
 ALTER TABLE vulnerability_names RENAME TO vulnerability_names_old;
 ALTER TABLE vulnerability_names_new RENAME TO vulnerability_names;
@@ -57,9 +50,6 @@ DROP TABLE vulnerability_names_old;
 COMMIT;
 EOF
 
-echo "Cleaning up old vulnerabilities..."
-$PSQL "DELETE FROM vulnerabilities WHERE name NOT IN (SELECT name FROM vulnerability_names);"
-
 rm -rf osv $temp_file $temp_names
 
-echo "Database update complete."
+echo "Database ready."
