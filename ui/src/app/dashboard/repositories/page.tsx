8fccbf3208dac1f34fae2f1291f75bdf013c4c2f
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-10 01:17:32 +0100

    Base idea for list

--- ui/src/app/dashboard/repositories/page.tsx
+++ ui/src/app/dashboard/repositories/page.tsx
@@ -1,8 +1,12 @@
-export default function Repositories() {
+import fetchRepositories from "@/utils/fetchRepositories"
+
+export default async function Repositories() {
+    const repositories = await fetchRepositories()
     return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-white p-4">
-        <h1 className="text-3xl font-bold text-blue-600">Repositories</h1>
-        <p className="mt-2 text-gray-700">List over repositories in Artifactory.</p>
-      </main>
-    );
-  }  
+        <main className="flex min-h-screen flex-col items-center justify-center bg-white p-4">
+            <h1 className="text-3xl font-bold text-blue-600">Repositories</h1>
+            <p className="mt-2 text-gray-700">List over repositories in Artifactory.</p>
+            {repositories.map((repository: any) => <div>{JSON.stringify(repository)}</div>)}
+        </main>
+    )
+}
---
+++ ui/src/utils/fetchRepositories.ts
@@ -0,0 +1,14 @@
+export default async function fetchRepositories() {
+    try {
+        const response = await fetch("")
+
+        if (!response.ok) {
+            throw new Error(await response.text())
+        }
+
+        const data = response.json()
+        return data
+    } catch (error) {
+        console.error(error)
+    }
+}
