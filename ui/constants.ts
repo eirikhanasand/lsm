a252e25b0d13d567de5d8928a7023d35429bf683
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-10 19:52:31 +0100

    Fixed env variable issue

--- ui/constants.ts
+++ ui/constants.ts
@@ -3,9 +3,8 @@ import dotenv from 'dotenv'
 dotenv.config({path: '../.env'})
 
 const { JFROG_ID: ENV_JFROG_ID, JFROG_TOKEN: ENV_JFROG_TOKEN } = process.env
-
 if (!ENV_JFROG_ID || !ENV_JFROG_TOKEN) {
-    throw new Error("Missing ENV_JFROG_ID")
+    throw new Error("Missing JFROG_ID or JFROG_TOKEN env variables.")
 }
 
 export const JFROG_ID = ENV_JFROG_ID
