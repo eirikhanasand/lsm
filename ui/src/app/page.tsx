73c9c0beed6d8c8f3b8b76774d48efd210768c63
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-11 00:44:23 +0100

    Added note for login

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -6,10 +6,11 @@ export default function Home() {
             <h1 className="text-3xl font-bold text-blue-600">Library Safety Manager</h1>
             <p className="mt-2 text-gray-700">A plugin for JFrog&apos;s Artifactory to check for vulnerabilities in packages.</p>
             
-            <Link href="/dashboard">
-                <button className="mt-4 rounded-lg bg-blue-500 px-6 py-2 text-white hover:bg-blue-600">
+            <Link href="/dashboard" className="grid place-items-center">
+                <button className="mt-2 rounded-lg bg-blue-500 px-6 py-2 text-white hover:bg-blue-600 mb-2">
                     Login
                 </button>
+                <h1 className="text-red-500">Login is currently disabled.</h1>
             </Link>
         </main>
     )
