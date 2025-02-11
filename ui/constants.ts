2e3512d230b6b407f4c85f0a3a96b56acd39788a
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-11 02:05:18 +0100

    Added API env variable

--- ui/constants.ts
+++ ui/constants.ts
@@ -2,10 +2,15 @@ import dotenv from 'dotenv'
 
 dotenv.config({path: '../.env'})
 
-const { JFROG_ID: ENV_JFROG_ID, JFROG_TOKEN: ENV_JFROG_TOKEN } = process.env
-if (!ENV_JFROG_ID || !ENV_JFROG_TOKEN) {
-    throw new Error("Missing JFROG_ID or JFROG_TOKEN env variables.")
+const { 
+    JFROG_ID: ENV_JFROG_ID, 
+    JFROG_TOKEN: ENV_JFROG_TOKEN,
+    API: ENV_API
+} = process.env
+if (!ENV_JFROG_ID || !ENV_JFROG_TOKEN || !ENV_API) {
+    throw new Error("Missing JFROG_ID, JFROG_TOKEN or API env variables.")
 }
 
 export const JFROG_ID = ENV_JFROG_ID
 export const JFROG_TOKEN = ENV_JFROG_TOKEN
+export const API = ENV_API
