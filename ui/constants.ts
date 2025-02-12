d7fd65b83e03221571da472373736477c72e3b10
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-12 23:19:56 +0100

    Fixed client side check issue

--- ui/constants.ts
+++ ui/constants.ts
@@ -7,10 +7,12 @@ const {
     JFROG_TOKEN: ENV_JFROG_TOKEN,
     API: ENV_API
 } = process.env
-if (!ENV_JFROG_ID || !ENV_JFROG_TOKEN || !ENV_API) {
+
+// Document check to bypass client side check
+if (!document && (!ENV_JFROG_ID || !ENV_JFROG_TOKEN || !ENV_API)) {
     throw new Error("Missing JFROG_ID, JFROG_TOKEN or API env variables.")
 }
 
 export const JFROG_ID = ENV_JFROG_ID
 export const JFROG_TOKEN = ENV_JFROG_TOKEN
-export const API = ENV_API
+export const API = ENV_API || 'localhost:8080'
