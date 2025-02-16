4e59d4f34904be472538a1cf8023c25379e73c5e
sindre <sindrhst@stud.ntnu.no>
2025-02-16 13:59:56 +0100

    Fleshed out post, delete and put functions.

--- ui/src/utils/filtering/postPackage.ts
+++ ui/src/utils/filtering/postPackage.ts
@@ -2,11 +2,20 @@ import { API } from "@constants"
 
 type PostListProps = {
     list: 'whitelist' | 'blacklist'
+    ecosystem: string
+    version: string
+    name: string
 }
 
-export default async function postPackage({list}: PostListProps) {
+export default async function postPackage({list, ecosystem, version, name}: PostListProps) {
     try {
-        const response = await fetch(`${API}/${list}`)
+        const response = await fetch(`${API}/${list}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ecosystem, version, name})
+        })
 
         if (!response.ok) {
             throw new Error(await response.text())
--- ui/src/utils/filtering/putPackage.ts
+++ ui/src/utils/filtering/putPackage.ts
@@ -2,11 +2,21 @@ import { API } from "@constants"
 
 type PutListProps = {
     list: 'whitelist' | 'blacklist'
+    name: string
+    oldVersion: string
+    newVersion: string
+    ecosystem: string
 }
 
-export default async function putPackage({list}: PutListProps) {
+export default async function putPackage({list, name, oldVersion, newVersion, ecosystem}: PutListProps) {
     try {
-        const response = await fetch(`${API}/${list}`)
+        const response = await fetch(`${API}/${list}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ecosystem, oldVersion, newVersion, name})
+        })
 
         if (!response.ok) {
             throw new Error(await response.text())
