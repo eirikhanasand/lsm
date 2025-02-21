2fc3d9b6b78bb322df2f14d6b600be11b1b7973a
eirikhanasand <eirik.hanasand@gmail.com>
2025-02-21 19:02:49 +0100

    replaced docker loopback with the servers address

--- ui/constants.ts
+++ ui/constants.ts
@@ -15,7 +15,7 @@ const {
 
 export const JFROG_ID = ENV_JFROG_ID
 export const JFROG_TOKEN = ENV_JFROG_TOKEN
-export const API = ENV_API || 'http://172.17.0.1:8080/api'
+export const API = ENV_API || 'http://129.241.150.86:8080/api'
 export const SERVER_API = "http://172.17.0.1:8080/api"
 export const ECOSYSTEMS = [  
     "ansible",
