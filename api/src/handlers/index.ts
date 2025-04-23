0e134c9e5597c185e71d02d6f511a5da41d10610
eirikhanasand <eirik.hanasand@gmail.com>
2025-04-23 15:31:24 +0200

    lsm -> library safety manager

---
+++ api/src/handlers/index.ts
@@ -0,0 +1,6 @@
+import { FastifyReply, FastifyRequest } from "fastify"
+
+export default async function IndexHandler(request: FastifyRequest, _: FastifyReply) {
+    const routes = request.server.printRoutes({ commonPrefix: false })
+    return `Welcome to the Library Safety Manager!\n\nValid routes are:\n${routes}`
+}
