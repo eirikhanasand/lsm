f251cb7048d38410d4c1d16e0239cbdf0dbd773c
eirikhanasand <eirik.hanasand@gmail.com>
2025-05-06 02:08:00 +0200

    Improved UX (sizing and displayed text)

--- ui/src/components/global/dropdown.tsx
+++ ui/src/components/global/dropdown.tsx
@@ -13,20 +13,33 @@ export default function Dropdown({
     allItems,
     setItems
 }: DropdownProps) {
+    const current = items.length 
+        ? JSON.stringify(items) === '[""]' ? `All ${item}` : items.join(', ') 
+        : `Select ${item}`
+
+    function handleAdd(e: React.ChangeEvent<HTMLSelectElement>) {
+        if (e.target.value === '') {
+            setItems([''])
+        } else {
+            setItems(Array.from(e.target.selectedOptions, (option) => option.value))
+        }
+    }
+
+
     return (
         <div className={`relative group inline-block w-full ${className}`}>
-            <div className={`${items.length ? 'text-foreground' : 'text-shallow'} h-7 p-1 border border-dark rounded-lg text-sm bg-light w-full cursor-pointer select-none h-[4vh] flex items-center pl-2`}>
-                {items.length ? items.join(', ') : `Select ${item}`}
+            <div className={`${items.length ? 'text-foreground' : 'text-shallow'} p-2 border border-dark rounded-lg text-sm bg-light w-full cursor-pointer select-none p-3 flex items-center`}>
+                {current}
             </div>
 
             <div className='absolute left-0 top-full hidden group-hover:block bg-light border border-dark rounded-lg z-10'>
                 <select
                     multiple={true}
                     value={items}
-                    onChange={(e) => setItems(Array.from(e.target.selectedOptions, (option) => option.value))}
+                    onChange={handleAdd}
                     className='p-1 text-sm text-foreground rounded-lg focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 h-40'
                 >
-                    <option value=''>All {item}</option>
+                    <option value="">All {item}</option>
                     {allItems.map((item) => (
                         <option key={item} value={item}>
                             {item}
