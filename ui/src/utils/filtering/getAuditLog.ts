ef8869766606bf106d6f2188e661c1125fa5689c
eirikhanasand <eirik.hanasand@gmail.com>
2025-05-06 02:00:54 +0200

    Added paging

--- ui/src/utils/filtering/getAuditLog.ts
+++ ui/src/utils/filtering/getAuditLog.ts
@@ -1,11 +1,24 @@
 import config from '@constants'
 
-const { SERVER_API } = config
+const { SERVER_API, DEFAULT_RESULTS_PER_PAGE } = config
+
+type AuditLogProps = {
+    side: 'client' | 'server'
+    page?: string
+    resultsPerPage?: string
+    search?: string
+}
 
 // Fetches the audit log from lsm API
-export default async function getAuditLog() {
+export default async function getAuditLog({side, page, resultsPerPage, search}: AuditLogProps) {
     try {
-        const response = await fetch(`${SERVER_API}/audit`)
+        console.log(`Fetching audit log: side=${side} page=${page}, resultsPerPage=${resultsPerPage}, search=${search}`)
+        const params = new URLSearchParams({ 
+            page: page || '1',
+            resultsPerPage:  resultsPerPage || String(DEFAULT_RESULTS_PER_PAGE || 50),
+            search: search || ''
+        })
+        const response = await fetch(`${side === 'server' ? SERVER_API : process.env.NEXT_PUBLIC_API}/audit?${params}`)
 
         if (!response.ok) {
             throw new Error(await response.text())
